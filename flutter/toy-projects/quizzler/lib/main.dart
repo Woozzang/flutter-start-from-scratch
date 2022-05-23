1771e3dd5261773ab88d9fa983feaf0142ce76cd
Oskar <woozzaang@gmail.com>
2022-05-23 15:49:23 +0900

    Complete quizzler app - add new package - Leverage Class

--- flutter/toy-projects/quizzler/lib/main.dart
+++ flutter/toy-projects/quizzler/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
+import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
+QuizBrain quizBrain = QuizBrain();
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
@@ -27,19 +31,41 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnser = quizBrain.getQuestionAnswer();
 
-  List<bool> answers = [
-    false,
-    true,
-    true,
-  ];
+    setState(() {
+      if (userPickedAnswer == correctAnser) {
+        if (quizBrain.isFinished()) {
+          Alert(
+                  context: context,
+                  title: 'Finished',
+                  desc: "You've just solved all quiz.")
+              .show();
+          quizBrain.reset();
+          scoreKeeper = [];
+          return;
+        }
 
-  int questionNumberIndex = 0;
+        scoreKeeper.add(Icon(
+          Icons.check,
+          color: Colors.green,
+        ));
+      } else {
+        scoreKeeper.add(Icon(
+          Icons.close,
+          color: Colors.red,
+        ));
+      }
+
+      quizBrain.nextQuestion();
+    });
+  }
+
+  void showAlert() {
+    Alert(context: context, title: "RFLUTTER", desc: "Flutter is awesome.")
+        .show();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +79,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumberIndex],
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -78,18 +104,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
-                bool correctAnswer = answers[questionNumberIndex];
-
-                if (correctAnswer == true) {
-                  print('user got it right');
-                } else {
-                  print('user got it wrong');
-                }
-
-                setState(() {
-                  questionNumberIndex++;
-                });
+                checkAnswer(true);
               },
             ),
           ),
@@ -109,17 +124,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer = answers[questionNumberIndex];
-
-                if (correctAnswer == false) {
-                  print('user got it right');
-                } else {
-                  print('user got it wrong');
-                }
-                //The user picked false.
-                setState(() {
-                  questionNumberIndex++;
-                });
+                checkAnswer(false);
               },
             ),
           ),
---
+++ flutter/toy-projects/quizzler/lib/question.dart
@@ -0,0 +1,8 @@
+class Question {
+  // Non-nullable instance filed must be initialized.
+  String qusetionText;
+  bool questionAnswer;
+
+  // The parameter 'q' can't have a value of 'null' becaue of it's type
+  Question(this.qusetionText, this.questionAnswer);
+}
